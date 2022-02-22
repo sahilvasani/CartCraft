757afa0a98e662c46d15fd3d9934d5ff971b2092
vivek613 <vaddoriyavivek666@gmail.com>
2022-02-22 18:22:09 +0530

    first

--- cartcraft_frontend/src/App.js
+++ cartcraft_frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Registration from "./pages/Registration";
 import SignIn from "./pages/SignIn";
+import ForgetPassword from "./components/ForgetPassword";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Routes>
           <Route path="/registration" element={<Registration />} />
           <Route path="/login" element={<SignIn />} />
+          <Route path="/forgetpassword" element={<ForgetPassword />} />
         </Routes>
       </BrowserRouter>
     </>
